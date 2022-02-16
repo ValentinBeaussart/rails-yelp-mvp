bfc80337e184e2d1cb488801893b4f6beac9acf9
Valentin Beaussart <valentin.beaussart@gmail.com>
2022-02-16 15:26:45 +0100

    exos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'restaurants/new'
+  # get 'restaurants/show'
+  # get 'restaurants/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :edit, :show] do
+    resources :reviews, only: [:index, :create]
+  end
 end
